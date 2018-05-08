851264c1673fe91c9444b11e8cf632a1565615c9
Mario Mendoza <mario.amg03@gmail.com>
2018-05-08 13:41:55 -0400

    Proyecto listo para la expo
    Proyecto listo

---
+++ movimiento de imagenes expo/movimiento de imagenes expo/Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox11 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox12 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox13 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox14 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox14, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        resources.ApplyResources(Me.PictureBox1, "PictureBox1")
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        resources.ApplyResources(Me.PictureBox2, "PictureBox2")
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.Transparent
+        resources.ApplyResources(Me.PictureBox4, "PictureBox4")
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        resources.ApplyResources(Me.PictureBox5, "PictureBox5")
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 10
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.BackColor = System.Drawing.Color.Transparent
+        resources.ApplyResources(Me.PictureBox6, "PictureBox6")
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.TabStop = False
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 2000
+        '
+        'PictureBox3
+        '
+        resources.ApplyResources(Me.PictureBox3, "PictureBox3")
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox7
+        '
+        resources.ApplyResources(Me.PictureBox7, "PictureBox7")
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox8
+        '
+        resources.ApplyResources(Me.PictureBox8, "PictureBox8")
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox9
+        '
+        resources.ApplyResources(Me.PictureBox9, "PictureBox9")
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.TabStop = False
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.ForeColor = System.Drawing.Color.LightCoral
+        Me.Label1.Name = "Label1"
+        '
+        'PictureBox10
+        '
+        resources.ApplyResources(Me.PictureBox10, "PictureBox10")
+        Me.PictureBox10.Name = "PictureBox10"
+        Me.PictureBox10.TabStop = False
+        '
+        'PictureBox11
+        '
+        resources.ApplyResources(Me.PictureBox11, "PictureBox11")
+        Me.PictureBox11.Name = "PictureBox11"
+        Me.PictureBox11.TabStop = False
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Name = "Label2"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'Timer3
+        '
+        Me.Timer3.Enabled = True
+        Me.Timer3.Interval = 1000
+        '
+        'Timer4
+        '
+        Me.Timer4.Enabled = True
+        Me.Timer4.Interval = 1
+        '
+        'PictureBox12
+        '
+        Me.PictureBox12.BackColor = System.Drawing.Color.Transparent
+        resources.ApplyResources(Me.PictureBox12, "PictureBox12")
+        Me.PictureBox12.Name = "PictureBox12"
+        Me.PictureBox12.TabStop = False
+        '
+        'PictureBox13
+        '
+        resources.ApplyResources(Me.PictureBox13, "PictureBox13")
+        Me.PictureBox13.Name = "PictureBox13"
+        Me.PictureBox13.TabStop = False
+        '
+        'PictureBox14
+        '
+        resources.ApplyResources(Me.PictureBox14, "PictureBox14")
+        Me.PictureBox14.Name = "PictureBox14"
+        Me.PictureBox14.TabStop = False
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Controls.Add(Me.PictureBox14)
+        Me.Controls.Add(Me.PictureBox13)
+        Me.Controls.Add(Me.PictureBox12)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox11)
+        Me.Controls.Add(Me.PictureBox10)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.Name = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Gray
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox14, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents PictureBox6 As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox7 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox8 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox9 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox10 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox11 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Timer3 As System.Windows.Forms.Timer
+    Friend WithEvents Timer4 As System.Windows.Forms.Timer
+    Friend WithEvents PictureBox12 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox13 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox14 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ movimiento de imagenes expo/movimiento de imagenes expo/Form1.vb
@@ -0,0 +1,245 @@
+﻿Public Class Form1
+    Dim cont As Byte = 0
+
+    Dim Vidas As Byte = 3
+
+    Dim Tiempo As Integer = 60
+
+    Dim X As Double = 720
+    Dim X2 As Double = 902
+    Dim X3 As Double = 782
+    Dim X4 As Double = 848
+    Dim Y2 As Double = 116
+    Dim Y3 As Double = 164
+
+    Dim Y As Double = 208
+    Dim Z As Double = 76
+    Dim W As Double = 195
+
+
+    Private Sub Form1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles Me.KeyPress
+
+
+        Select Case e.KeyChar
+            Case "w"
+                Timer2.Enabled = False
+
+                PictureBox1.Visible = True
+                PictureBox2.Visible = False
+                PictureBox4.Visible = False
+                PictureBox4.Enabled = False
+                PictureBox6.Visible = False
+
+
+                For i As Byte = 1 To 100
+                    PictureBox1.Top -= 1
+                    If PictureBox1.Location.Y < 0 Then
+                        PictureBox1.Location = New Point(Val(PictureBox1.Location.X), 0)
+                    End If
+                    System.Threading.Thread.Sleep(8)
+                    My.Application.DoEvents()
+                Next
+
+                For i As Byte = 1 To 100
+                    PictureBox1.Top += 1
+                    If PictureBox1.Location.Y > 198 Then
+                        PictureBox1.Location = New Point(Val(PictureBox1.Location.X), 198)
+                    End If
+                    System.Threading.Thread.Sleep(8)
+                    My.Application.DoEvents()
+                Next
+
+                Timer2.Enabled = True
+
+
+            Case "d"
+                If Z < 660 Then
+                    Z = Z + 5
+                End If
+                Timer2.Enabled = False
+                PictureBox1.Visible = True
+                PictureBox2.Visible = False
+
+                PictureBox4.Enabled = False
+                PictureBox4.Visible = False
+
+                PictureBox6.Visible = False
+                PictureBox1.Location = New Point(Z, W)
+                PictureBox2.Location = New Point(Z, 195)
+
+                PictureBox4.Location = New Point(Z, 174)
+                PictureBox6.Location = New Point(Z, 195)
+                System.Threading.Thread.Sleep(10)
+                My.Application.DoEvents()
+
+                Timer2.Enabled = True
+            Case "a"
+                If Z > 0 Then
+                    Z = Z - 5
+                End If
+                Timer2.Enabled = False
+
+
+                PictureBox1.Visible = True
+                PictureBox2.Visible = False
+
+                PictureBox4.Enabled = False
+                PictureBox4.Visible = False
+
+                PictureBox6.Visible = False
+                PictureBox1.Location = New Point(Z, 195)
+                PictureBox2.Location = New Point(Z, 195)
+
+                PictureBox4.Location = New Point(Z, 174)
+                PictureBox6.Location = New Point(Z, 195)
+                System.Threading.Thread.Sleep(10)
+                My.Application.DoEvents()
+                Timer2.Enabled = True
+
+            Case "f"
+
+                PictureBox1.Visible = False
+
+                PictureBox2.Visible = True
+                PictureBox4.Visible = False
+                PictureBox4.Enabled = False
+                PictureBox6.Visible = False
+                For i As Byte = 1 To 75
+                    If Z < 660 Then
+                        Z = Z + 4
+                    End If
+                    PictureBox1.Location = New Point(Z, 195)
+                    PictureBox2.Location = New Point(Z, 195)
+
+                    PictureBox4.Location = New Point(Z, 174)
+                    PictureBox6.Location = New Point(Z, 195)
+                    System.Threading.Thread.Sleep(10)
+                    My.Application.DoEvents()
+                    If X < Z + 120 Then
+                        X = 720
+                    End If
+
+                    If X2 < Z + 120 Then
+                        X2 = 902
+                    End If
+                Next
+
+            Case "q"
+                PictureBox1.Visible = False
+                PictureBox2.Visible = False
+
+                PictureBox4.Visible = True
+                PictureBox4.Enabled = True
+                PictureBox6.Visible = False
+
+                If X < Z + 120 Then
+                    X = 720
+                End If
+
+                If X2 < Z + 120 Then
+                    X2 = 902
+                End If
+
+
+        End Select
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim Ruta As String
+        Ruta = My.Application.Info.DirectoryPath
+        Dim A As System.Media.SoundPlayer
+        A = New System.Media.SoundPlayer(Ruta + "\MC.WAV")
+        A.Play()
+
+
+        PictureBox1.Visible = False
+        PictureBox2.Visible = False
+        PictureBox4.Visible = False
+
+        PictureBox13.Visible = False
+        PictureBox14.Visible = False
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        X = X - 2
+        X2 = X2 - 2
+        X3 = X3 - 3
+        X4 = X4 - 5
+
+        PictureBox5.Location = New Point(X, Y)
+        PictureBox10.Location = New Point(X2, 195)
+        PictureBox3.Location = New Point(X3, 116)
+        PictureBox11.Location = New Point(X4, 164)
+
+
+        If X < -50 Then
+            X = 720
+            Vidas = Vidas - 1
+        End If
+
+        If X2 < -50 Then
+            X2 = 902
+            Vidas = Vidas - 1
+        End If
+
+        If X3 < -50 Then
+            X3 = 782
+
+        End If
+
+        If X4 < -50 Then
+            X4 = 848
+
+        End If
+
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        PictureBox6.Visible = True
+        PictureBox4.Visible = False
+        PictureBox1.Visible = False
+        PictureBox2.Visible = False
+    End Sub
+
+    Private Sub Timer3_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer3.Tick
+        Label3.Text = Tiempo & "s"
+        Tiempo = Tiempo - 1
+
+        If Tiempo = 0 Then
+            PictureBox14.Visible = True
+            Timer1.Enabled = False
+            Timer3.Enabled = False
+            Timer4.Enabled = False
+
+        End If
+    End Sub
+
+    Private Sub Timer4_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer4.Tick
+
+
+        If Vidas = 2 Then
+            PictureBox7.Visible = False
+        End If
+
+        If Vidas = 1 Then
+            PictureBox8.Visible = False
+        End If
+
+        If Vidas = 0 Then
+            PictureBox9.Visible = False
+            Timer1.Enabled = False
+            Timer3.Enabled = False
+            Timer4.Enabled = False
+            PictureBox13.Visible = True
+        End If
+
+
+
+    End Sub
+    Private Sub PictureBox14_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox14.Click
+
+    End Sub
+End Class
